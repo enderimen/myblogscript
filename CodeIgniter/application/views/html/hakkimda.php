ea61d829dfca66f83301e156c18c25d22b22d445
Ender IMEN <enderimen@hotmail.com>
2017-05-29 22:55:06 +0300

    About me page is designed.

---
+++ CodeIgniter/application/views/html/hakkimda.php
@@ -0,0 +1,32 @@
+<?php include "header.php"; ?>
+
+<div class="container">
+
+	<div class="content">
+		<div class="panel panel-info">
+
+		<!--Profil resmi-->
+		<div class="row">
+			<div class="profil-image text-center">
+				<img src="../img/profil-img.jpg" alt="" class="img-circle" style="width: 250px;height: 250px">
+			</div>
+		</div>
+		<!--Profil bitti-->
+
+
+		<!--Hakkımda-->	
+		<div class="panel-heading"><h3>Ben Kimim?</h3></div>
+		<div class="panel-body">
+			<p>Mayıs 1995 yılında dünyaya gözlerimi istanbul kartalda açtım. 
+			
+			</p>
+		</div>
+		<!--Hakkımda bitti-->	
+		</div>
+	</div>
+
+</div>
+
+<?php include "footer.php"; ?>
+
+
