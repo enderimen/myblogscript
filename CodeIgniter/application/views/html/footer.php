e197a633cded7c8d1bb2cb9f090a4a3544715910
Ender IMEN <enderimen@hotmail.com>
2017-05-30 03:58:29 +0300

    footer links have been edited.

--- CodeIgniter/application/views/html/footer.php
+++ CodeIgniter/application/views/html/footer.php
@@ -1,15 +1,16 @@
-<footer style="background-color: #37474F">
+<footer style="background-color: #37474F;margin-top: 20px;">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-2" style="margin-top:15px; ">
+				<div class="col-sm-2 pull-left" style="margin-top:40px; ">
 					<ul>
-						<a href="index.php"><li class="list">Anasayfa</li></a>
-						<a href="hakkimda.php"><li class="list">Ben Kimim?</li></a>
-						<a href="iletisim.php" ><li class="list">İletişim</li></a>
+						<a href="index.php"><li class="list"><span class="glyphicon glyphicon-home"> Anasayfa</li></a>
+						<a href="hakkimda.php"><li class="list"><span class="glyphicon glyphicon-user"> Ben Kimim?</li></a>
+						<a href="iletisim.php" ><li class="list"><span class="glyphicon glyphicon-envelope"> İletişim</li></a>
 					</ul>
 				</div>
-				<div class="col-sm-4 pull-right">
-					<h4>Sosyal Ağ <span class="glyphicon glyphicon-hand-right"></span></h4>
+
+				<div class="content col-sm-2 pull-right">
+					<h4 style="color: orange">Sosyal Ağ <span class="glyphicon glyphicon-bookmark"></span></h4>
 					<ul>
 						<a href="https://www.facebook.com/enderimen" target="_blank"><li class="list">Facebook</li></a>
 						<a href="https://www.twitter.com/imenender" target="_blank"><li class="list">Twitter</li></a>
