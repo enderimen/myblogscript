b8cdf8cf3e63defe45c162c2c88e50c714ba3ae7
Ender IMEN <enderimen@hotmail.com>
2017-05-30 15:49:24 +0300

    Contact page has been edited.

--- CodeIgniter/application/views/html/iletisim.php
+++ CodeIgniter/application/views/html/iletisim.php
@@ -4,7 +4,7 @@
 <div class="container">
 	<div class="panel-heading text-center"><div class="panel-title"><h3 style="font-family: tahoma;">Mesaj Gönder</h3> </div></div>
 	
-	<div class="left col-lg-3 col-md-3 col-sm-6 col-xs-12"></div>
+	<div class="left col-lg-3 col-md-3 col-sm-6 col-xs-12"><!--Void div--></div>
 	
 	<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 		<div class="panel" style="margin-top:50px;">
