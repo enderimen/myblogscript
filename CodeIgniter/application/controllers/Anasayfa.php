dd7709019227caad3736124edae01e7d891d78ea
Ender IMEN <enderimen@hotmail.com>
2017-06-13 13:10:17 +0300

    some files have been moved.

---
+++ CodeIgniter/application/controllers/Anasayfa.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Anasayfa extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('anasayfa');
+
+		//Git view de adresi verilen sayfayı görüntüle
+		/*Burası view yüklemek için kullanılan bölümdür.*/
+		/*Burada çağıracağımız sayfa ilk olarak başlatılan sayfa olacaktır.[views içerindeki dosyayının yolunu veriyoruz.]*/
+	}
+}
---
+++ CodeIgniter/application/views/hakkimda.php
@@ -0,0 +1,33 @@
+<?php include "header.php"; ?>
+
+<div class="container">
+
+	<div class="content" style="margin-top: 50px;">
+
+		<!--Profil resmi-->
+		<div class="panel panel-info">
+		<div class="row">
+			<div class="profil-image text-center">
+				<img src="../img/profil-img.jpg" alt="" class="img-circle" style="width: 250px;height: 250px">
+			</div>
+		</div>
+		<!--Profil bitti-->
+
+
+		<!--Hakkımda-->	
+		<div class="panel-heading"><h3>Ben Kimim?</h3></div>
+		<div class="panel-body">
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mayıs 1995 istanbul doğumluyum.İlk öğretim ve orta öğretim öğrenimimi sultanbeyli Atatürk ilk öğretim okulunda tamamladım.
+			İşte bilgisayarlarla altı ya da yedinci sınıfta ilk defa orada tanıştım.Evimizde bilgisayar yok tabi.Gerek duyduğumuzda okula gidip ödevlerimzi yapıyorduk onun haricinde zaten oyun felan oynatmıyorlardı.Bu şekilde çok sürmeden eski, sigara dumanından sararmış bir bilgisayar hediye edildi(Eniştem Sağolsun).Her neyse lise tercihleri felan derken kocaeli de yatılı okuma kararı verdim.Ve Yıldız entegre ticaret meslek lisesinde bigisayar bölümünü,dal olarakta veritabanı dalını seçtim.Okul dersleri çok hoşuma gitmişti en çokta programlama kısmı baktım yapabiliyorum.Üniversitede de nasip olursa ya bilgisayar mühendisliği ya da yazılım mühendisliği bölümünü seçecektim.Yılın sonunda kesin olarak yazılım mühendisliğini seçmeye karar verdim.İşte sınavlara girdik felan derken Karadeniz Teknik Üniiversitesi Yazılım Mühendisliğini kazanmışım.(Ama fakültemiz trabzon merkezde değil OF 'ta :) )Şuan 2.sınıf 'tayım. 1.sınıftan itibaren yazılım kulübünde aktif üyeyim aynı zamanda da kulüp başkanlığını devraldım.Bir yıl kadar da biz yürüteceğiz nasip olursa.Bu bloğu yazma amacıma gelirsem bizim üniversitemiz teknik üni. olduğu için neredeyse her yaz stajımız var ve bu bulunmaz nimet bizim için.Bende bu güzel deneyimlerimi,stajda öğrendiklerimi bir nebzede olsa pekiştirmek ve yazılarımı okuyacak olan sizlere paylaşmak adına bu bloğu yazma kararı verdim ve sıfırdan kodladım.Son olarakta bu aralar Front-end 'e sardım diyebilirim bakalım ne kadar gidecek.<b>Sağlıcakla kalın,Takipte kalın...</b> 
+			
+			</p>
+		</div>
+		<!--Hakkımda bitti-->	
+		</div>
+	</div>
+
+</div>
+
+<?php include "footer.php"; ?>
+
+
