70d2e1a733892eff7f1ba664261324047eba0547
Ender IMEN <enderimen@hotmail.com>
2017-06-15 16:35:55 +0300

    homepage controller edited.

--- application/controllers/Anasayfa.php
+++ application/controllers/Anasayfa.php
@@ -4,12 +4,17 @@ class Anasayfa extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('/include/header'); 
-		$this->load->view('/include/slider');
 		$this->load->view("anasayfa");
-		$this->load->view('/include/footer');
 		//Git view de adresi verilen sayfayı görüntüle
 		/*Burası view yüklemek için kullanılan bölümdür.*/
 		/*Burada çağıracağımız sayfa ilk olarak başlatılan sayfa olacaktır.[views içerindeki dosyayının yolunu veriyoruz.]*/
+	}
+	public function hakkimda()
+	{
+		$this->load->view('/hakkimda');		
+	}
+	public function iletisim()
+	{
+		$this->load->view('/iletisim');
 	}
 }
