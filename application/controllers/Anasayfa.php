a451ec29df04f5385424d7aaf2cc5d948ecc543e
Ender IMEN <enderimen@hotmail.com>
2017-06-21 19:44:07 +0300

    The articles were added to the tab as a title.

--- application/controllers/Anasayfa.php
+++ application/controllers/Anasayfa.php
@@ -7,16 +7,23 @@ class Anasayfa extends CI_Controller {
 		$this->load->model('vt');
 		$sonuc=$this->vt->paylasimcek();
 		
+		$sayi=$this->vt->paylasimsayisi();
+
 		$data=new stdclass;
 		$data->bilgi=$sonuc;
 
+		$this->session->set_userdata('title','Blog | Ender İMEN');//session başlattık sekme başlığı için
 		$this->load->view("anasayfa",$data);
 		//Git view de adresi verilen sayfayı görüntüle
 		/*Burası view yüklemek için kullanılan bölümdür.*/
 		/*Burada çağıracağımız sayfa ilk olarak başlatılan sayfa olacaktır.[views içerindeki dosyayının yolunu veriyoruz.]*/
 	}
+
+	//Sayfa içi yönlendirme[Hakkımda sayfası]
+
 	public function hakkimda()
 	{
+		//Database den veirtabanına bilgileri gönderiyoruz.
 		$this->load->model('vt');
 		$sonuc=$this->vt->hakkindabilgi();
 		
@@ -25,6 +32,8 @@ class Anasayfa extends CI_Controller {
 
 		$this->load->view('/hakkimda',$data);		
 	}
+
+	//Sayfa içi yönlendirme[İletişim sayfası]
 	public function iletisim()
 	{
 		$this->load->view('/iletisim');
