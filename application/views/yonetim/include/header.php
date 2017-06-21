8b799882f6fd04126dcaeabf7069cfdd1262fd48
Ender IMEN <enderimen@hotmail.com>
2017-06-21 20:01:31 +0300

    The links in the management panel menu are arranged.

--- application/views/yonetim/include/header.php
+++ application/views/yonetim/include/header.php
@@ -14,8 +14,8 @@
 							<li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="<?php  echo base_url(); ?>/assets/backend/assets/img/profil-img.jpg" style="width: 24px;height: 24px;" class="img-circle" alt="Avatar"> <span>Ender İMEN</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
 								<ul class="dropdown-menu">
-									<li><a href="profilim"><i class="lnr lnr-user"></i> <span>Profilimi Gör</span></a></li>
-									<li><a href="index"><i class="lnr lnr-exit"></i> <span>Çıkış</span></a></li>
+									<li><a href="<?php echo base_url('yonetimpaneli/profilim'); ?>"><i class="lnr lnr-user"></i> <span>Profilimi Gör</span></a></li>
+									<li><a href="<?php echo base_url('yonetim/cikis'); ?>"><i class="lnr lnr-exit"></i> <span>Çıkış Yap</span></a></li>
 								</ul>
 							</li>
 						</ul>
--- application/views/yonetim/include/sidebar.php
+++ application/views/yonetim/include/sidebar.php
@@ -1,3 +1,4 @@
+<?php $info=$this->session->userdata('info'); //Session olan adminin bilgilerini çektik ?> 
 <!doctype html>
 <html lang="en">
 
@@ -14,6 +15,7 @@
 	<link rel="stylesheet" href="<?php  echo base_url(); ?>assets/backend/assets/css/demo.css">
 	<!-- ICONS -->
 	<link rel="shortcut icon" href="..\img\.ico" type="image/x-icon" /><!--Logo çekilecek-->
+	<script src="<?php echo base_url('assets/backend/ckeditor/ckeditor.js') ?>"></script><!--Editor düzenleyicisi-->
 </head>
 
 <body>
@@ -22,18 +24,17 @@
 		<!-- SIDEBAR -->
 		<div class="sidebar">
 			<div class="brand">
-				
-				<a href="index.php"><img alt="BLOG" title="Anasayfaya Git" class="img-responsive logo"></a><b style="color:#00A0F0">Web Master</b>
-				<h5 style="color:white">Ender İMEN | 31.05.2017 <i class="fa fa-thumbs-o-up"></i></h5>
+				<a href="<?php echo base_url('yonetimpaneli'); ?>"><img alt="BLOG" title="Anasayfaya Git" class="img-responsive logo"></a><b style="color:#00A0F0">Web Master</b>
+				<h5 style="color:white"><?php echo $info->admin_adi_soyadi; //Admin adını ve soyadını yazdırıyoruz?> | 31.05.2017 <i class="fa fa-thumbs-o-up"></i></h5>
 			</div>
 			<div class="sidebar-scroll">
 				<nav>
 					<ul class="nav">
-						<li><a href="anasayfa" class="active"><i class="lnr lnr-bookmark"></i> <span>Tüm Gönderiler</span></a></li>
-						<li><a href="paylasim"><i class="lnr lnr-pencil"></i><span>Gönderi Paylaş</span></a></li>
-						<li><a href="footer"><i class="lnr lnr-magic-wand"></i><span>Footer Düzenle</span></a></li>
-						<li><a href="slider"><i class="lnr lnr-magic-wand"></i><span>Slider Düzenle</span></a></li>
-						<li><a href="hakkinda"><i class="lnr lnr-magic-wand"></i><span>Hakkında Düzenle</span></a></li>
+						<li><a href="<?php echo base_url('yonetimpaneli'); ?>" class="active"><i class="lnr lnr-bookmark"></i> <span>Tüm Gönderiler</span></a></li>
+						<li><a href="<?php echo base_url('yonetimpaneli/paylasim'); ?>"><i class="lnr lnr-pencil"></i><span>Gönderi Paylaş</span></a></li>
+						<li><a href="<?php echo base_url('yonetimpaneli/footer'); ?>"><i class="lnr lnr-magic-wand"></i><span>Footer Düzenle</span></a></li>
+						<li><a href="<?php echo base_url('yonetimpaneli/slider'); ?>"><i class="lnr lnr-magic-wand"></i><span>Slider Düzenle</span></a></li>
+						<li><a href="<?php echo base_url('yonetimpaneli/hakkinda');echo '/1' ?>"><i class="lnr lnr-magic-wand"></i><span>Hakkında Düzenle</span></a></li>
 					</ul>
 				</nav>
 			</div>
