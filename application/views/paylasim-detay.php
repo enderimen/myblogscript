e9a46c333c7639be0bcba55ff0cc0e81da40eabe
Ender IMEN <enderimen@hotmail.com>
2017-06-23 04:51:15 +0300

    "share-detail-page" edited.

--- application/views/paylasim-detay.php
+++ application/views/paylasim-detay.php
@@ -3,7 +3,6 @@
 
 <div class="container">	
 	<div class="left col-lg-3 col-md-3 col-sm-6 col-xs-12"><!--Void div left--></div>
-		<div class="body">
 			<div class="col-lg-12 col-md-6 col-sm-12 col-xs-12">
 					
 					<!--Paylaşım Başlık-->
@@ -13,9 +12,10 @@
 						<!--Paylaşım Başlık-->
 						<?php echo $bilgi->paylasim_yazisi; ?>
 						<?php echo "<br/><b style='color:gray'>Paylaşım Tarihi</b>:  ".$bilgi->paylasim_tarih; ?>				
-					</div>
-			</div>	
-		</div>
+					</div><br><br><br><br>
+			</div>
 </div>
-
+<a href="#" class="up">
+	<img src="<?php echo base_url();?>assets/frontend/img/up.png" style="width: 40px;height: 40px;" alt="">
+</a>
 <?php $this->load->view('include/footer.php'); ?>
