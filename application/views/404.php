ac6d6c36118e11750e7e1e8631e42005148bf560
Ender IMEN <enderimen@hotmail.com>
2017-06-28 17:31:46 +0300

    404 sayfa bulunamadı.Sayfa çağırma kodları yazıldı.

---
+++ application/views/404.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" class="fullscreen-bg">
+
+<head>
+	<title>Blog | Sayfa Bulunamadı</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+	
+	<link rel="shortcut icon" href="<?php echo base_url();?>assets/frontend/img/logo.ico" type="image/x-icon" />
+	<!-- CSS -->
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/frontend/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/frontend/css/bootstrap-theme.css">
+
+	<!-- ICONS -->
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url();?>assets/frontend/img/apple-icon.png">
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<div class="vertical-align-wrap">
+			<div class="vertical-align-middle">
+				<div class="auth-box lockscreen clearfix" style="margin-top: 10%">
+					<div class="content">
+						<div class="logo text-center">
+							<h4 class="text-center">Blog | Ender İMEN</h4>
+							<img src="<?php echo base_url();?>assets/frontend/img/logo.png" alt="Blog Logo">
+						</div>
+						<div class="user text-center">
+							
+							<form action="">
+								<div class="panel">
+									<h3>Aradığınız Sayfa Bulunamadı!</h3>
+									<a href="<?php echo base_url('anasayfa'); ?>">
+										<input type="button" class="btn btn-info" value="Anasayfaya Git">
+									</a>
+								</div>
+							</form>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+</body>
+
+</html>
